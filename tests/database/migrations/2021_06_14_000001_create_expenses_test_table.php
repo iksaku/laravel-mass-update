3dc9755b8011c32acac816c2a171148e314989d3
Jorge González <yo@jorgeglz.io>
2021-07-10 01:32:33 -0500

    Rollback migrations to traditional classes. Only use linux runners for those docker services

--- tests/database/migrations/2021_06_14_000001_create_expenses_test_table.php
+++ tests/database/migrations/2021_06_14_000001_create_expenses_test_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+class CreateExpensesTestTable extends Migration {
     public function up(): void
     {
         Schema::create('expenses', function (Blueprint $table) {
@@ -25,4 +25,4 @@ return new class extends Migration {
     {
         Schema::drop('expenses');
     }
-};
+}
